3f7b9f69b3fceb3166f63c92452e4a94a32fc6d0
humkobhiseekhnahai <113964544+humkobhiseekhnahai@users.noreply.github.com>
2024-06-06 11:38:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import "./index.css"
+import SideBar from './components/sidebar'
+
+
+
+function App() {
+
+
+  return (
+    <div className='w-screen h-screen bg-gradient-to-tr from-lime-200 from-0% via-neutral-900 via-25% to-teal-500'>
+      {/* background div */}
+      <div className='grid grid-cols-[10vh_165vh] grid-rows-[10vh_89vh]'>
+        <SideBar/>
+        <div className='col-start-2 row-start-2 rounded-md  backdrop-blur-xl bg-neutral-900/70 drop-shadow-xl border-neutral-900 border-2'>
+          {/* card component */}
+          
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
